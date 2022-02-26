cd8933afa258f8bf23b6d458fa247547de9ccf2b
Richard Lim <richardlimka@gmail.com>
2022-02-26 22:49:38 +0800

    Updated vpc tags.

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "demo_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
-    Name = "${var.project_name} VPC"
+    Name = "${var.project_name}"
   }
 }
 
